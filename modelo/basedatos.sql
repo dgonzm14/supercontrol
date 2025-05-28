55eaadcf4bd31599e2dd879b2f4b32ea39514eb3
Diego González <dgonzm14@estudiantes.unileon.es>
2025-05-28 15:36:46 +0200

    commit largo

--- modelo/basedatos.sql
+++ modelo/basedatos.sql
@@ -66,25 +66,8 @@ CREATE TABLE precios (
     FOREIGN KEY (id_producto) REFERENCES productos(id_producto)
 );
 
-CREATE TABLE ventas (
-    id_venta INT PRIMARY KEY IDENTITY(1,1),
-    id_cliente INT,
-    fecha_venta DATE,
-    total DECIMAL(10, 2),
-    FOREIGN KEY (id_cliente) REFERENCES clientes(id_cliente)
-);
 
-CREATE TABLE detalle_ventas (
-    id_detalle INT PRIMARY KEY IDENTITY(1,1),
-    id_venta INT,
-    id_producto INT,
-    cantidad INT,
-    precio DECIMAL(10, 2),
-    FOREIGN KEY (id_venta) REFERENCES ventas(id_venta),
-    FOREIGN KEY (id_producto) REFERENCES productos(id_producto)
-);
 
--- Crear la tabla de usuarios para login (añadiendo email y contraseña)
 CREATE TABLE usuarios (
     id_usuario INT PRIMARY KEY IDENTITY(1,1),
     nombre_usuario VARCHAR(100),
